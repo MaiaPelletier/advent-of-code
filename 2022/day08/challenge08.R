528559daff27dfbeff8adfaf1419e92c3705d089
MaiaPelletier <maia.pelletier15@gmail.com>
2022-12-08 18:55:52 -0500

    wrong answer

--- 2022/day08/challenge08.R
+++ 2022/day08/challenge08.R
@@ -1,65 +1,42 @@
-# library(stringr)
 library(purrr)
 
 # read input
-input <- readLines(file.path("2022", "day08", "example.txt"))
-input
+input <- readLines(file.path("2022", "day08", "input.txt"))
 
 tree_grid <- map(strsplit(input, ""), as.numeric)
-outer_trees <- (length(tree_grid) - 1) * (length(tree_grid[[1]]) - 1)
+visible_trees <- (length(tree_grid) - 1) * (length(tree_grid[[1]]) - 1) # these are the outer trees
 
 grid_len <- length(tree_grid[[1]])
 grid_hgt <- length(tree_grid)
 
-# between 2 and length - 1
-# i <- 2
-# tree <- tree_grid[[3]][i]
-# 
-# 
-# down <- unlist(map(tree_grid[(i+1):grid_hgt], i))
-# up <- unlist(map(tree_grid[1:i], i))
-# 
-# left <- tree_grid[[3]][1:(i-1)]
-# right <- tree_grid[[3]][(i+1):grid_len]
-# 
-# any(c(
-#   all(tree > down),
-#   all(tree > up),
-#   all(tree > right),
-#   all(tree > left)
-# ))
-# 
 test <- c()
 
 for (i in seq(2, grid_len-1)) {
   
-  for (j in seq(2, grid_hgt-1))
-  
-  # tree <- tree_grid[[j]][i]
-  print(paste0("tree", "[", i, ",", j, "]"))
-  
-  down <- unlist(map(tree_grid[(i+1):grid_hgt], i))
-  up <- unlist(map(tree_grid[1:i], i))
-  
-  left <- tree_grid[[3]][1:(i-1)]
-  right <- tree_grid[[3]][(i+1):grid_len]
-  
-  visible <- any(
-    c(
-      all(tree_grid[[j]][i] > down),
-      all(tree_grid[[j]][i] > up),
-      all(tree_grid[[j]][i] > right),
-      all(tree_grid[[j]][i] > left)
+  for (j in seq(2, grid_hgt-1)) {
+    
+    down <- unlist(map(tree_grid[(i+1):grid_hgt], i))
+    up <- unlist(map(tree_grid[1:i], i))
+    
+    left <- tree_grid[[j]][1:(i-1)]
+    right <- tree_grid[[j]][(i+1):grid_len]
+    
+    visible <- any(
+      c(
+        all(tree_grid[[j]][i] > down),
+        all(tree_grid[[j]][i] > up),
+        all(tree_grid[[j]][i] > right),
+        all(tree_grid[[j]][i] > left)
+      )
     )
-  )
-  
-  print(visible)
-  
-  test <- c(test, visible)
+    
+    if (visible) visible_trees <- visible_trees + 1
+    
+  }
   
 }
 
-
+visible_trees
 
 
 
