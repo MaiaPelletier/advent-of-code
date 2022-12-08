1741efbfaabb16a6e56568062e193d44950f06aa
Maia Pelletier <maia.pelletier15@gmail.com>
2022-12-08 13:11:58 -0500

    day 8 wip

--- 2022/day08/challenge08.R
+++ 2022/day08/challenge08.R
@@ -11,35 +11,32 @@ outer_trees <- (length(tree_grid) - 1) * (length(tree_grid[[1]]) - 1)
 grid_len <- length(tree_grid[[1]])
 grid_hgt <- length(tree_grid)
 
-tree_check <- function(tree) {
-  
-  
-  
-}
-
 # between 2 and length - 1
-i <- 2
-tree <- tree_grid[[3]][i]
-
-
-down <- unlist(map(tree_grid[(i+1):grid_hgt], i))
-up <- unlist(map(tree_grid[1:i], i))
-
-left <- tree_grid[[3]][1:(i-1)]
-right <- tree_grid[[3]][(i+1):grid_len]
-
-any(c(
-  all(tree > down),
-  all(tree > up),
-  all(tree > right),
-  all(tree > left)
-))
+# i <- 2
+# tree <- tree_grid[[3]][i]
+# 
+# 
+# down <- unlist(map(tree_grid[(i+1):grid_hgt], i))
+# up <- unlist(map(tree_grid[1:i], i))
+# 
+# left <- tree_grid[[3]][1:(i-1)]
+# right <- tree_grid[[3]][(i+1):grid_len]
+# 
+# any(c(
+#   all(tree > down),
+#   all(tree > up),
+#   all(tree > right),
+#   all(tree > left)
+# ))
+# 
+test <- c()
 
 for (i in seq(2, grid_len-1)) {
   
   for (j in seq(2, grid_hgt-1))
   
   # tree <- tree_grid[[j]][i]
+  print(paste0("tree", "[", i, ",", j, "]"))
   
   down <- unlist(map(tree_grid[(i+1):grid_hgt], i))
   up <- unlist(map(tree_grid[1:i], i))
@@ -47,12 +44,18 @@ for (i in seq(2, grid_len-1)) {
   left <- tree_grid[[3]][1:(i-1)]
   right <- tree_grid[[3]][(i+1):grid_len]
   
-  any(c(
-    all(tree_grid[[j]][i] > down),
-    all(tree_grid[[j]][i] > up),
-    all(tree_grid[[j]][i] > right),
-    all(tree_grid[[j]][i] > left)
-  ))
+  visible <- any(
+    c(
+      all(tree_grid[[j]][i] > down),
+      all(tree_grid[[j]][i] > up),
+      all(tree_grid[[j]][i] > right),
+      all(tree_grid[[j]][i] > left)
+    )
+  )
+  
+  print(visible)
+  
+  test <- c(test, visible)
   
 }
 
