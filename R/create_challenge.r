ee86a39e1967e8069375db80d3fd14f03f2ee0e6
Maia Pelletier <44327546+MaiaPelletier@users.noreply.github.com>
2024-12-12 11:18:48 -0500

    update new challenge function

--- R/create_challenge.r
+++ R/create_challenge.r
@@ -1,31 +1,26 @@
 library(here)
 library(glue)
 
+create_file <- function(file) {
+  if (!file.exists(file)) {
+    file.create(file)
+  }
+}
+
 new_challenge <- function(year, day, language = "R") {
-    if (day %in% 1:9) {
-        day <- glue("0{day}")
-    }
+  if (day %in% 1:9) {
+    day <- glue("0{day}")
+  }
 
-    dir <- here(year, glue("day_{day}"))
-    ex_file <- here(year, glue("day_{day}"), "example.txt")
-    input_file <- here(year, glue("day_{day}"), "input.txt")
-    challenge_file <- here(year, glue("day_{day}"), glue("day_{day}.{language}"))
+  dir <- here(year, glue("day_{day}"))
+  ex_file <- here(year, glue("day_{day}"), "example.txt")
+  input_file <- here(year, glue("day_{day}"), "input.txt")
+  challenge_file <- here(year, glue("day_{day}"), glue("day_{day}.{language}"))
+  blank_files <- list(ex_file, input_file, challenge_file)
 
-    if (!dir.exists(dir)) {
-        dir.create(dir)
-    }
+  dir.create(dir)
+  purrr::walk(blank_files, create_file)
 
-    if (!file.exists(ex_file)) {
-        file.create(ex_file)
-    }
-
-    if (!file.exists(input_file)) {
-        file.create(input_file)
-    }
-
-    if (!file.exists(challenge_file)) {
-        file.create(challenge_file)
-    }
 }
 
 # new_challenge(2024, 2)
