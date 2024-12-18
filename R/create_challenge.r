17001b1bdcc7b8c9312084d2dcbfaa448774b86a
Maia Pelletier <44327546+MaiaPelletier@users.noreply.github.com>
2024-12-18 08:54:59 -0500

    update gitignore and create challenge function to make readmes

--- R/create_challenge.r
+++ R/create_challenge.r
@@ -1,5 +1,3 @@
-library(here)
-library(glue)
 
 create_file <- function(file) {
   if (!file.exists(file)) {
@@ -7,19 +5,34 @@ create_file <- function(file) {
   }
 }
 
+rmarkdown_template <- function(year, day) {
+  glue::glue("
+  ---
+  title: \"{year} - Day {day}\"
+  output: github_document
+  ---
+
+  ```{r setup, include=FALSE}
+  knitr::opts_chunk$set(echo = TRUE, message = FALSE, warning = FALSE)
+  ```
+  ")
+}
+
 new_challenge <- function(year, day, language = "R") {
   if (day %in% 1:9) {
-    day <- glue("0{day}")
+    day <- glue::glue("0{day}")
   }
 
-  dir <- here(year, glue("day_{day}"))
-  ex_file <- here(year, glue("day_{day}"), "example.txt")
-  input_file <- here(year, glue("day_{day}"), "input.txt")
-  challenge_file <- here(year, glue("day_{day}"), glue("day_{day}.{language}"))
+  dir <- here::here(year, glue::glue("day_{day}"))
+  ex_file <- here::here(year, glue::glue("day_{day}"), "example.txt")
+  input_file <- here::here(year, glue::glue("day_{day}"), "input.txt")
+  challenge_file <- here::here(year, glue::glue("day_{day}"), glue::glue("day_{day}.{language}"))
+  readme_file <- here::here(year, glue::glue("day_{day}"), "README.Rmd")
   blank_files <- list(ex_file, input_file, challenge_file)
 
   dir.create(dir)
   purrr::walk(blank_files, create_file)
+  readr::write_lines(rmarkdown_template(year, day), readme_file)
 
 }
 
